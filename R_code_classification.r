a649c5ce3d779f7300d701e9f6a29b6c2a51e437
MaiaGuerra <100956822+MaiaGuerra@users.noreply.github.com>
2022-04-21 17:45:21 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -25,3 +25,38 @@ plot(soc$map, col=cl) # plottiamo solo la mappa del modello, visualizzando le 3 
 # perchè l'algoritmo raggruppa in modo casuale i pixel dell'immagine nelle 3 categorie che abbiamo impostato,
 # prendendo casualmente 10000 pixel sul totale dei pixel dell'immagine (7669050 pixel) ma in maniera diversa ogni volta che viene utilizzato.
 # L'algoritmo mantiene però il pattern spaziale dell'immagine (se il numero di categorie in cui è stata divisa rimane lo stesso)
+
+
+
+# 21/04/2022 Analisi immagini Grand Canyon
+
+setwd("C:/lab/")
+library(raster)
+library(RStoolbox)
+
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg") # gc = grand canyon
+gc
+
+
+plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+dev.off()
+# visualizziamo un'immagine molto simile a quella reale
+# di solito le bande sono r=3, g=2, b=3, ma essendo già elaborata perchè è un'immagine landsat,
+# si possono visualizzare anche come lo abbiamo impostato noi
+
+# cambiamo lo stretch per avere una colorazione più differenziata
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+dev.off()
+
+# classificazione non supervisionata (Unsupervised Classification)
+# clustering di dati raster basato su k gruppi
+gcclass2 <- unsuperClass(gc, nClasses=2)
+gcclass2
+
+# visualizziamo la mappa creata con due classi
+plot(gcclass2$map)
+dev.off()
+
+# visualizziamo quella con quattro
+gcclass4 <- unsuperClass(gc, nClasses=4)
+plot(gcclass4$map)
